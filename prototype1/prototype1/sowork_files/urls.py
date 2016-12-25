d81e9d639c84817648da8476abf0f7124c3d18bf
yihuaqi <yihuaqi@qq.com>
2016-12-25 17:15:00 +0800

    Add a form that can upload file

--- prototype1/prototype1/sowork_files/urls.py
+++ prototype1/prototype1/sowork_files/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'sowork_filesZ'
+app_name = 'sowork_files'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^upload/', views.upload_file, name='upload_file'),
+    url(r'^download/', views.download_file, name='download_file'),
 ] 
--- prototype1/prototype1/sowork_files/views.py
+++ prototype1/prototype1/sowork_files/views.py
@@ -4,14 +4,19 @@ from .forms import UploadFileForm
 from .models import *
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'sowork_files/index.html')
 
 def upload_file(request):
 	if request.method == 'POST':
+		print("upload_file - POST ")
 		form = UploadFileForm(request.POST, request.FILES)
 		if form.is_valid():
+			print("upload_file - POST is_valid")
 			instance = FileModel(file=request.FILES['file'], file_name="uploaded_file")
 			return HttpResponse("Upload success")
-		else:
-			form = UploadFileForm()
-		return render(request, 'upload.html', {'form', form})
+	else:
+		form = UploadFileForm()
+	return render(request, 'sowork_files/upload.html', {'form': form})
+
+def download_file(requst):
+	return render(request, 'sowork_files/index.html')
