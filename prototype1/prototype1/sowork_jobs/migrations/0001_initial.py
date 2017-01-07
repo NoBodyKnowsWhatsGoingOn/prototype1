3b840df9785e93e69ec60b015c1e241a24551d1e
yihuaqi <yihuaqi@qq.com>
2017-01-07 15:23:59 +0800

    Fix migration conflicts

--- prototype1/prototype1/sowork_jobs/migrations/0001_initial.py
+++ prototype1/prototype1/sowork_jobs/migrations/0001_initial.py
@@ -14,11 +14,14 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='FileModel',
+            name='JobInfo',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('file_name', models.CharField(max_length=100)),
-                ('file', models.FileField(upload_to=b'')),
+                ('title', models.CharField(default='Title', max_length=100)),
+                ('description', models.CharField(default='Description', max_length=500)),
+                ('createTime', models.DateTimeField(editable=False)),
+                ('lastUpdateTime', models.DateTimeField(editable=False)),
+                ('active', models.BooleanField(default=False)),
             ],
         ),
     ]
