cbd3c25560b7f363ecac50269265579da78fdbc6
genji <gamem.8818@gmail.com>
2017-01-11 15:47:28 -0800

    add css to post jobs page

--- prototype1/prototype1/sowork_jobs/forms.py
+++ prototype1/prototype1/sowork_jobs/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class PostJobForm(forms.Form):
-    title = forms.CharField(label='Job Title', max_length=100)
-    description = forms.CharField(label='Job Description', max_length=500)
-    active = forms.NullBooleanField()
+    title = forms.CharField(label='Job Title', max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    description = forms.CharField(label='Job Description', max_length=500, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    active = forms.BooleanField()
