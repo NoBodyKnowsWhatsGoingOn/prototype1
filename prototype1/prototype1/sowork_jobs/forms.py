69f7496e2e9e1ea186f2d64337ef18d0097d176b
genji <gamem.8818@gmail.com>
2017-01-11 16:17:43 -0800

    update post jobs page css

--- prototype1/prototype1/sowork_jobs/forms.py
+++ prototype1/prototype1/sowork_jobs/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class PostJobForm(forms.Form):
     title = forms.CharField(label='Job Title', max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control'}))
     description = forms.CharField(label='Job Description', max_length=500, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    active = forms.BooleanField()
+    active = forms.BooleanField(required=False)
