4cafd03cd0876e077772c024d246a887df2cc61b
genji <gamem.8818@gmail.com>
2017-01-17 12:56:32 -0800

    change layout of job_detail

--- prototype1/prototype1/comments/forms.py
+++ prototype1/prototype1/comments/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class CommentForm(forms.Form):
-    body = forms.CharField(label='Comment Description', max_length=500)
+    body = forms.CharField(label='Comment Description', max_length=500, widget=forms.TextInput(attrs={'class': 'form-control'}))
     # todo: add attachments
